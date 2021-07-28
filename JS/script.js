8a344f658a1fa2c0f6d0c5a7f2c62c86eb7f85c8
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-07-28 17:37:39 +0200

    style

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,12 @@
 // 1.Chiedi all’utente il suo nome
-var firstname=prompt("Your name");
+// var firstname=prompt("Your name");
 
 // 2.poi chiedi il suo cognome
-var surname=prompt("Your surname");
+// var surname=prompt("Your surname");
 
 // 3.poi chiedi il suo colore preferito
-var color=prompt("Your favourite color");
+// var color=prompt("Your favourite color");
 
 // 4.Infine scrivi sulla pagina nomecognomecolorepreferito40
-document.write(firstname += surname += color);
-document.write("classe-40")
+// document.write(firstname += surname += color);
+// document.write("classe-40")
