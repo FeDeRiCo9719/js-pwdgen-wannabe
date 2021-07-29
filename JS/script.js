4e5b9fe3517824abc417b204167667d9d3ed5743
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-07-29 10:27:13 +0200

    fix

--- JS/script.js
+++ JS/script.js
@@ -8,7 +8,7 @@ var surname = prompt("Your surname");
 var color = prompt("Your favourite color");
 
 // 4.Crea la password
-var password = (firstname + surname + color +"classe 40")
+var password = (firstname + surname + color + "classe 40");
 
 // 5.Infine scrivi sulla pagina nomecognomecolorepreferito40
-document.write(password)
+document.getElementById("password").innerHTML += password;
